738bba9d3924af1ce59277f9ab0166d8aee04631
amitshukla12345 <amitshukl22509@gmail.com>
2024-12-12 12:38:14 +0530

    second commit

--- src/main/java/com/example/RailwaysBookingSystem/Entity/Train.java
+++ src/main/java/com/example/RailwaysBookingSystem/Entity/Train.java
@@ -1,4 +1,4 @@
-package com.example.RailwaysBookingSystem.Train;
+package com.example.RailwaysBookingSystem.Entity;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
--- src/main/java/com/example/RailwaysBookingSystem/Respository/TrainRepository.java
+++ src/main/java/com/example/RailwaysBookingSystem/Respository/TrainRepository.java
@@ -1,9 +1,9 @@
-package com.example.RailwaysBookingSystem.Repository;
+package com.example.RailwaysBookingSystem.Respository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.yaml.snakeyaml.events.Event.ID;
 
-import com.example.RailwaysBookingSystem.Train.Train;
+import com.example.RailwaysBookingSystem.Entity.Train;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
 public interface TrainRepository extends JpaRepository<Train, Integer> {
